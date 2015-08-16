dcf9662488cc99410e6d0897e6db453d2e5991a2
Julian Brendl <julianbrendl@gmail.com>
2015-08-17 00:48:55 +0200

    fixed maxFiles in client config

--- config/client/client_config.js
+++ config/client/client_config.js
@@ -8,6 +8,6 @@ gOptions = {
     name: 'Smith',       // Sets the name of the website from FamShare to SmithShare here for example (if enabled)
 
     // Dropzone values -- used by the file upload API (dropzone) -- more config options are in js/api/dropzone.js
-    maxFilesize: 100500000,     //*, In mega byte
+    maxFilesize: 10000,     //*, In mega byte
     maxFiles: 100               //*
 };
