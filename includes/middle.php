2e9836e5effa664c96b55f97a62550de93e89845
Julian Brendl <julianbrendl@gmail.com>
2015-08-16 23:17:51 +0200

    pushed images for readme

--- includes/middle.php
+++
@@ -1,5 +0,0 @@
-</head>
-<body class="unselectable">
-<div id="wrapper">
-<div id="content_wrapper">
-<div id="content_inner_wrapper">
--- js/includes/header.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function() {
-    if (gOptions.enabled) {
-        $('#title').text(gOptions.name + "Share");
-    }
-});
