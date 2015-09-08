6b23cbbc9447bb4d1245e09ddbcd8f887497ca29
Julian Brendl <julianbrendl@gmail.com>
2015-09-08 23:34:12 +0200

    refactored a bit

--- config/client/client_config.js
+++ config/client/client_config.js
@@ -8,9 +8,9 @@ gOptions = {
     name: 'Smith',       // Sets the name of the website from FamShare to SmithShare here for example (if enabled)
 
     // Dropzone values -- used by the file upload API (dropzone) -- more config options are in js/api/dropzone.js
-    maxFilesize: 10000,     //*, In mega byte
-    maxFiles: 1000,         //*
-    parallelUploads: 10,  //*
-    resumableUpload: false   //*, Set to true if resumable uploads should be used. If your browser does not support it,
-    // you can disable the resumable upload by setting it to false.
+    maxFilesize: 10000,         //*, In mega byte
+    maxFiles: 1000,             //*
+    parallelUploads: 10,        //*
+    resumableUpload: true       //*, Set to true if resumable uploads should be used. If your browser does not support
+                                // it, you can disable the resumable upload by setting it to false.
 };
